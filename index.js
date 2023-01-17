582c472e2f5bfdb2f09cfe42ae1b81adb6950ee0
dsaza <dayrondaza05@gmail.com>
2023-01-17 15:32:11 -0500

    Upgrade index.ts to js

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
-console.log('Codermk')
+import path from 'node:path'
+import fs from 'node:fs'
+import bootstrap from './src/bootstrap.js'
+
+async function main() {
+	const config = JSON.parse(fs.readFileSync(path.join(process.cwd(), './codermk.json')))
+	const mode = process.argv[2]
+
+	bootstrap(mode, config)
+}
+
+main()
